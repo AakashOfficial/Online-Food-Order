6615940197d00ec07484891d6a56c126fedb6e88
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-29 17:53:14 +0530

    Food Model Class !

---
+++ OnlineFoodOrder/src/main/java/com/tyagi/project/OnlineFoodOrder/model/Food.java
@@ -0,0 +1,84 @@
+package com.tyagi.project.OnlineFoodOrder.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import org.springframework.web.multipart.MultipartFile;
+
+@Entity
+@Table
+public class Food {
+
+	@Id
+	@GeneratedValue
+	int foodid;
+
+	String foodname;
+	int catid;
+	int quantity;
+	int price;
+	String fooddesc;
+
+	@Transient
+	MultipartFile pimage;
+
+	public int getFoodid() {
+		return foodid;
+	}
+
+	public void setFoodid(int foodid) {
+		this.foodid = foodid;
+	}
+
+	public String getFoodname() {
+		return foodname;
+	}
+
+	public void setFoodname(String foodname) {
+		this.foodname = foodname;
+	}
+
+	public int getCatid() {
+		return catid;
+	}
+
+	public void setCatid(int catid) {
+		this.catid = catid;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getFooddesc() {
+		return fooddesc;
+	}
+
+	public void setFooddesc(String fooddesc) {
+		this.fooddesc = fooddesc;
+	}
+
+	public MultipartFile getPimage() {
+		return pimage;
+	}
+
+	public void setPimage(MultipartFile pimage) {
+		this.pimage = pimage;
+	}
+
+}
