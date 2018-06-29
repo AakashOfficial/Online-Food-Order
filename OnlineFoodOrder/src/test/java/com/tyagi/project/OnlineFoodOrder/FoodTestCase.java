4f9115ae661ec13aed2f0020e425442eb086d121
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-29 18:14:22 +0530

    Food Add Test Case Done !

---
+++ OnlineFoodOrder/src/test/java/com/tyagi/project/OnlineFoodOrder/FoodTestCase.java
@@ -0,0 +1,31 @@
+package com.tyagi.project.OnlineFoodOrder;
+
+import javax.naming.Context;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.tyagi.project.OnlineFoodOrder.dao.FoodDAO;
+import com.tyagi.project.OnlineFoodOrder.model.Food;
+
+public class FoodTestCase {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.tyagi.project.OnlineFoodOrder");
+		context.refresh();
+
+		FoodDAO foodDAO = (FoodDAO) context.getBean("foodDAO");
+
+		Food food = new Food();
+
+		food.setFoodname("Pizza");
+		food.setQuantity(3);
+		food.setFooddesc("Delicious");
+
+		foodDAO.insertUpdateFood(food);
+		System.out.println("food Inserted");
+
+		
+
+	}
+}
+
