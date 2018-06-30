334fd9ed73b7338b9ba46969d1f724d37f71ce2d
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-06-30 09:10:37 +0530

    Retrieve Test Case Done !

--- OnlineFoodOrder/src/test/java/com/tyagi/project/OnlineFoodOrder/CartTestCase.java
+++ OnlineFoodOrder/src/test/java/com/tyagi/project/OnlineFoodOrder/CartTestCase.java
@@ -13,7 +13,10 @@ public class CartTestCase {
 		context.scan("com.tyagi.project.OnlineFoodOrder");
 		context.refresh();
 		CartDAO cartDAO = (CartDAO) context.getBean("cartDAO");
-		Cart cart = new Cart();
+		
+		// Adding Or Inserting to Cart
+		
+		/*Cart cart = new Cart();
 		cart.setCartid(1001);
 		cart.setFoodid(3);
 		cart.setFoodname("Leggin");
@@ -22,10 +25,19 @@ public class CartTestCase {
 		cart.setStatus("N");
 		cart.setUsername("Anshika");
 		cartDAO.addToCart(cart);
-		System.out.println("Added To Cart");
+		System.out.println("Added To Cart");*/
 		
+		// Retrieve 
 		
-		
+		List<Cart> list = cartDAO.getCartItems("Anshika");
+
+		for (Cart cart2 : list) {
+			System.out.println(cart2.getCartid() + ";;;");
+			System.out.println(cart2.getFoodid() + ";;;");
+			System.out.println(cart2.getFoodname() + ";;;");
+			System.out.println(cart2.getPrice() + ";;;");
+			System.out.println(cart2.getQuantity() + ";;;");
+		}
 
 	}
 }
