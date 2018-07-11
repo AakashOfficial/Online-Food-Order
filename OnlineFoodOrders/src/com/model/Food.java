7f026f30ceb024217d38c0d529a56429ee1e2d56
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-11 22:25:34 +0530

    Food Model Class !

---
+++ OnlineFoodOrders/src/com/model/Food.java
@@ -0,0 +1,50 @@
+package com.model;
+
+import java.io.Serializable;
+
+public class Food implements Serializable {
+
+	private String food_name;
+	private String food_category;
+	private String food_price;
+	private String food_type;
+	private String food_desc;
+	
+	public String getFood_name() {
+		return food_name;
+	}
+	public void setFood_name(String food_name) {
+		this.food_name = food_name;
+	}
+	public String getFood_category() {
+		return food_category;
+	}
+	public void setFood_category(String food_category) {
+		this.food_category = food_category;
+	}
+	public String getFood_price() {
+		return food_price;
+	}
+	public void setFood_price(String food_price) {
+		this.food_price = food_price;
+	}
+	public String getFood_type() {
+		return food_type;
+	}
+	public void setFood_type(String food_type) {
+		this.food_type = food_type;
+	}
+	public String getFood_desc() {
+		return food_desc;
+	}
+	public void setFood_desc(String food_desc) {
+		this.food_desc = food_desc;
+	}
+	
+	@Override
+	public String toString() {
+		return "Food [food_name=" + food_name + ", food_category=" + food_category + ", food_price=" + food_price
+				+ ", food_type=" + food_type + ", food_desc=" + food_desc + "]";
+	}
+	
+}
