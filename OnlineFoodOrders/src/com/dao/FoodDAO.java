58f093c98d890ce05d598c1b39f22c03415a035a
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-16 21:04:28 +0530

    Food DAO !

---
+++ OnlineFoodOrders/src/com/dao/FoodDAO.java
@@ -0,0 +1,20 @@
+package com.dao;
+
+import com.model.Food;
+import com.model.User;
+
+public interface FoodDAO {
+
+	public boolean validateUser(User u);
+	public String giveRole(User u);
+	public boolean addUser(User u);
+	public boolean removeUser(User u);
+	public boolean updateUser(User u);
+	public boolean addFood(Food f);
+	public boolean removeFood(Food f);
+	public boolean removeFoodById(Food f);
+	public boolean updateFood(Food f);
+	public boolean showFood(Food f);
+	public boolean addToCart(User u,Food f);
+	
+}
