d7352054d072f554fc085a6363658e9dfb502958
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-16 21:12:35 +0530

    Add To Cart Servlet !

--- OnlineFoodOrders/src/com/tyagi/servlet/AddToCart.java
+++ OnlineFoodOrders/src/com/tyagi/servlet/AddToCart.java
@@ -1,5 +1,24 @@
 package com.tyagi.servlet;
 
-public class AddToCart {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/addToCart")
+public class AddToCart extends HttpServlet {
+
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		PrintWriter out =res.getWriter();
+		HttpSession hs = req.getSession();
+		String s1 =(String)hs.getAttribute("UID");
+		out.print(s1);
+		
+	}
 }
