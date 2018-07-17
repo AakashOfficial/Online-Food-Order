588c20ef355c939d5faef2d84487c34b0dcf797b
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-18 00:48:50 +0530

    Servlet For Showing The Cart !

---
+++ OnlineFoodOrders/src/com/tyagi/servlet/ShowCart.java
@@ -0,0 +1,18 @@
+package com.tyagi.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/showCart")
+public class ShowCart {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)throws ServletException,IOException {
+		
+		request.getRequestDispatcher("/ShowCart.jsp").forward(request, response);
+		
+	}
+}
